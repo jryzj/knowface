e566b9326f8d743a87335c22a6226cd77f87d2fb
jryzj <2381002887@qq.com>
2018-07-14 14:33:27 +0800

    Initial commit

---
+++ getToken.js
@@ -0,0 +1,20 @@
+var https = require('https');
+var qs = require('querystring');
+
+const param = qs.stringify({
+    'grant_type': 'client_credentials',
+    'client_id': '',
+    'client_secret': ''
+});
+
+https.get(
+    {
+        hostname: 'aip.baidubce.com',
+        path: '/oauth/2.0/token?' + param,
+        agent: false
+    },
+    function (res) {
+        // 在标准输出中查看运行结果
+        res.pipe(process.stdout);
+    }
+);/* Practice by JERRY, created on 2018/7/11*/
